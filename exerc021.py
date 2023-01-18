435e63f8549f864f29f09e8d7aa8b6ba3a54dcb4
Lucas dos Santos <sl.solucas@gmail.com>
2023-01-18 12:23:05 -0300

    Update exerc021.py

--- exerc021.py
+++ exerc021.py
@@ -3,5 +3,6 @@ from playsound import playsound
 Faça um programa em python que abra a reproduza 
 o áudio de um arquivo mp3
 """
+# Nesse exemplo você teria que ter o arquivo "exerc021.mp3" na mesma pasta do programa 
 
 playsound('exerc021.mp3')
